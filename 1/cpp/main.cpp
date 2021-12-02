e8a648fb2d46377780291b6e99dc12ae94c27111
Paterno <joao.1496.sjb@gmail.com>
2021-12-02 20:51:36 +0100

    first answer right.

--- 1/cpp/main.cpp
+++ 1/cpp/main.cpp
@@ -1,6 +1,29 @@
 #include <iostream>
+#include <fstream>
+#include <string>
 
 int main()
 {
+    std::ifstream file;
+    file.open("../input.txt");
+
+    uint32_t i_current, i_previous, count = 0;
+    std::string s_current;
+    i_previous = i_current;
+    file >> s_current;
+    i_current = stoi(s_current);
+    while (file)
+    {
+        i_previous = i_current;
+        file >> s_current;
+        i_current = stoi(s_current);
+        if (i_current > i_previous)
+        {
+            count++;
+        }
+    }
+
+    std::cout << count;
+
     return 0;
 }
