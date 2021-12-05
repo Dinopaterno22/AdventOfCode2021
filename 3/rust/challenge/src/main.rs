0c4c3353a0d65ea468fb313d894f123708a729ab
Paterno <joao.1496.sjb@gmail.com>
2021-12-05 23:32:09 +0100

    Challenge 3 complete.

--- 3/rust/challenge/src/main.rs
+++ 3/rust/challenge/src/main.rs
@@ -1,7 +1,7 @@
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 
-fn binary_to_int(value: &mut u64, vector: Vec<u32>) {
+fn _binary_to_int(value: &mut u64, vector: Vec<u32>) {
     for (index, _iter) in vector.iter().enumerate() {
         if _iter == &1 {
             let shift = 1 << index;
@@ -10,7 +10,7 @@ fn binary_to_int(value: &mut u64, vector: Vec<u32>) {
     }
 }
 
-fn main() {
+fn _first_part() {
     let filename = "../../input.txt";
     let file = File::open(filename).unwrap();
     let reader = BufReader::new(file);
@@ -48,10 +48,141 @@ fn main() {
             epsilon_vec.push(1);
         }
     }
-    binary_to_int(&mut gamma, gamma_vec);
-    binary_to_int(&mut epsilon, epsilon_vec);
+    _binary_to_int(&mut gamma, gamma_vec);
+    _binary_to_int(&mut epsilon, epsilon_vec);
 
     println!("Gamma = {} (Binary: {:012b})", gamma, gamma);
     println!("Epsilon = {} (Binary: {:012b})", epsilon, epsilon);
     println!("{}", gamma * epsilon);
+}
+
+fn second_part() {
+    let filename = "../../input.txt";
+    let file = File::open(filename).unwrap();
+    let reader = BufReader::new(file);
+    let _size: usize = 12;
+
+    let mut _bitcount: Vec<i64> = Vec::new();
+    let mut oxygen_word_vec: Vec<String> = Vec::new();
+    let mut carbon_word_vec: Vec<String> = Vec::new();
+    for _i in 0.._size {
+        _bitcount.push(0);
+    }
+    for (_index, line) in reader.lines().enumerate() {
+        let line = line.unwrap();
+        oxygen_word_vec.push(line);
+    }
+    carbon_word_vec.clone_from(&oxygen_word_vec);
+
+    for current_bit in 0.._size {
+        if &mut oxygen_word_vec.len() > &mut 1 {
+            for i in 0.._size {
+                _bitcount[i] = 0;
+            }
+            for (_index, word) in &mut oxygen_word_vec.iter().enumerate() {
+                for (i, c) in word.chars().enumerate() {
+                    match c {
+                        '1' => {
+                            _bitcount[i] += 1;
+                        }
+                        '0' => {
+                            _bitcount[i] -= 1;
+                        }
+                        _ => println!("Problem"),
+                    }
+                }
+            }
+            let bit: char = if _bitcount[current_bit] >= 0 {
+                '1'
+            } else {
+                '0'
+            };
+            let mut indices: Vec<usize> = Vec::new();
+            for (_index, word) in &mut oxygen_word_vec.iter().enumerate() {
+                for (i, c) in word.chars().enumerate() {
+                    if i == current_bit && c != bit {
+                        indices.push(_index);
+                    }
+                }
+            }
+            let mut removal: usize = 0;
+            for i in indices.iter() {
+                oxygen_word_vec.remove(*i - removal);
+                removal += 1;
+            }
+        }
+    }
+    for current_bit in 0.._size {
+        if &mut carbon_word_vec.len() > &mut 1 {
+            for i in 0.._size {
+                _bitcount[i] = 0;
+            }
+            for (_index, word) in &mut carbon_word_vec.iter().enumerate() {
+                for (i, c) in word.chars().enumerate() {
+                    match c {
+                        '1' => {
+                            _bitcount[i] += 1;
+                        }
+                        '0' => {
+                            _bitcount[i] -= 1;
+                        }
+                        _ => println!("Problem"),
+                    }
+                }
+            }
+            let bit: char = if _bitcount[current_bit] >= 0 {
+                '0'
+            } else {
+                '1'
+            };
+            let mut indices: Vec<usize> = Vec::new();
+            for (_index, word) in &mut carbon_word_vec.iter().enumerate() {
+                for (i, c) in word.chars().enumerate() {
+                    if i == current_bit && c != bit {
+                        indices.push(_index);
+                    }
+                }
+            }
+            let mut removal: usize = 0;
+            for i in indices.iter() {
+                carbon_word_vec.remove(*i - removal);
+                removal += 1;
+            }
+        }
+    }
+
+    let mut oxygen_binary_vector: Vec<u32> = Vec::new();
+    let mut carbon_binary_vector: Vec<u32> = Vec::new();
+
+    let oxygen_str: String = oxygen_word_vec[0].chars().rev().collect();
+    let carbon_str: String = carbon_word_vec[0].chars().rev().collect();
+
+    for (_i, c) in oxygen_str.chars().enumerate() {
+        if c == '1' {
+            oxygen_binary_vector.push(1);
+        } else {
+            oxygen_binary_vector.push(0);
+        }
+    }
+    for (_i, c) in carbon_str.chars().enumerate() {
+        if c == '1' {
+            carbon_binary_vector.push(1);
+        } else {
+            carbon_binary_vector.push(0);
+        }
+    }
+
+    let mut oxygen: u64 = 0;
+    let mut carbon: u64 = 0;
+
+    _binary_to_int(&mut oxygen, oxygen_binary_vector);
+    _binary_to_int(&mut carbon, carbon_binary_vector);
+    println!("Oxygen {} (Binary: {:#012b})", oxygen, oxygen);
+    println!("Carbon {} (Binary: {:#012b})", carbon, carbon);
+    println!("Result {}", oxygen * carbon);
+}
+
+fn main() {
+    _first_part();
+    second_part();
 }
