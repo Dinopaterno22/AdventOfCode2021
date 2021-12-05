68606b13a6e4139084dece5dcc7759e8834e9703
Paterno <joao.1496.sjb@gmail.com>
2021-12-05 17:19:32 +0100

    1st challenge 2nd part in rust

--- 1/rust/challenge/src/main.rs
+++ 1/rust/challenge/src/main.rs
@@ -1,18 +1,14 @@
-use std::env;
 use std::fs::File;
-use std::io::Read;
 use std::io::{BufRead, BufReader};
 
-fn main() {
+fn single_measurement() {
     let filename = "../../input.txt";
     let file = File::open(filename).unwrap();
     let reader = BufReader::new(file);
-    let mut _current: u32 = 0;
-    let mut _count: u32 = 0;
-    let mut _previous: u32 = 0;
-    let mut _first: u8 = 0;
 
-    for (index, line) in reader.lines().enumerate() {
+    let (mut _current, mut _count, mut _previous, mut _first): (u32, u32, u32, u8) = (0, 0, 0, 0);
+
+    for (_index, line) in reader.lines().enumerate() {
         let line = line.unwrap();
         if _first > 1 {
             _previous = _current;
@@ -24,5 +20,40 @@ fn main() {
             _first += 1;
         }
     }
-    println!("Final count: {}", _count);
+    println!("Final single count: {}", _count);
+}
+
+fn triple_measurement() {
+    let filename = "../../input.txt";
+    let file = File::open(filename).unwrap();
+    let reader = BufReader::new(file);
+    let (mut _a, mut _b, mut _c, mut _current, mut _previous, mut _count, mut _first): (
+        u32,
+        u32,
+        u32,
+        u32,
+        u32,
+        u32,
+        u8,
+    ) = (0, 0, 0, 0, 0, 0, 0);
+
+    for (_index, line) in reader.lines().enumerate() {
+        let line = line.unwrap();
+        _a = _b;
+        _b = _c;
+        _c = line.parse().unwrap();
+        _previous = _current;
+        _current = _a + _b + _c;
+        if _current > _previous && _first > 2 {
+            _count += 1;
+        } else if _first < 3 {
+            _first += 1;
+        }
+    }
+    println!("Final triple count: {}", _count);
+}
+
+fn main() {
+    single_measurement();
+    triple_measurement();
 }
