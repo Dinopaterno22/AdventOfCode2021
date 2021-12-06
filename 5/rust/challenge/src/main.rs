bf8c41a99d85c0e0e1382c4e65638468a8f57d35
Paterno <joao.1496.sjb@gmail.com>
2021-12-06 23:29:13 +0100

    updated 5.1

--- 5/rust/challenge/src/main.rs
+++ 5/rust/challenge/src/main.rs
@@ -26,15 +26,34 @@ fn split_coordinates(s: String) -> (Point, Point) {
 fn initialize_map(x_size: u32, y_size: u32) -> Vec<Vec<u32>> {
     let mut map: Vec<Vec<u32>> = Vec::new();
 
-    for x in 0..x_size {
+    for _x in 0..x_size {
         map.push(Vec::new());
-        for y in 0..y_size {
-            map[x][y].push(0);
+    }
+
+    for i in map.iter_mut() {
+        for _y in 0..y_size {
+            i.push(0);
         }
     }
 
     return map;
 }
+
+fn draw_line(map: Vec<Vec<u32>>, line: (Point, Point)) {
+    if line.0.x - line.1.x != 0 {
+        draw_horizontal_line(map, line);
+    } else if line.0.y - line.1.y != 0 {
+        draw_vertical_line();
+    }
+}
+
+fn draw_horizontal_line(map: Vec<Vec<u32>>, line: (Point, Point)) {
+    let y = line.0.y;
+
+    for i in line.0.x..line.1.x {}
+}
+
+fn draw_vertical_line() {}
 
 fn main() -> Result<(), Error> {
     let filename = "../../input_test.txt";
@@ -56,6 +75,7 @@ fn main() -> Result<(), Error> {
             y_size = y_max;
         }
 
+        // Disregard diagonals
         if points.0.x != points.1.x || points.0.y != points.1.y {
             continue;
         }
@@ -63,7 +83,7 @@ fn main() -> Result<(), Error> {
         lines.push(points);
     }
 
-    let map: Vec<Vec<u32>>;
+    let mut map: Vec<Vec<u32>> = initialize_map(x_size, y_size);
 
     Ok(())
 }
