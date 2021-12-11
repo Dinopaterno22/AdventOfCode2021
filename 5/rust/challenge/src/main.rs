2e70905def9cb949ccf39b652636989f755264af
Joao <joao.1496.sjb@gmail.com>
2021-12-11 20:09:42 +0100

    fixed methods. added them to Map Class.

--- 5/rust/challenge/src/main.rs
+++ 5/rust/challenge/src/main.rs
@@ -12,14 +12,16 @@ struct Point {
 struct PointMap {
     x_size: u32,
     y_size: u32,
+    step : u32,
     contents: Vec<u32>,
 }
 
 impl PointMap {
-    pub fn new(x_size: u32, y_size: u32) -> Self {
+    pub fn new(x: u32, y: u32) -> Self {
         return PointMap {
-            x_size: x_size,
-            y_size: y_size,
+            x_size: x,
+            y_size: y,
+            step: x,
             contents: Vec::new(),
         };
     }
@@ -31,9 +33,33 @@ impl PointMap {
         }
         return 0;
     }
-    fn set_point(self, x: u32, y: u32) {
+    fn set_point(&mut self, x: u32, y: u32) {
         let index: usize = (x * (y + 1)) as usize;
         self.contents[index] += 1;
+    }
+
+    fn _draw_line(&mut self, line: (Point, Point)) {
+        if line.0.x - line.1.x != 0 {
+            _draw_horizontal_line(self, line);
+        } else if line.0.y - line.1.y != 0 {
+            _draw_vertical_line(self, line);
+        }
+    }
+    
+    fn _draw_horizontal_line(&mut self, line: (Point, Point)) {
+        let y = line.0.y;
+    
+        for i in line.0.x..line.1.x {
+            self.set_point(i, y);
+        }
+    }
+    
+    fn _draw_vertical_line(&mut self, line: (Point, Point)) {
+        let x = line.0.x;
+    
+        for i in line.0.y..line.1.y {
+            self.set_point(x, i);
+        }
     }
 }
 
@@ -68,7 +94,7 @@ fn initialize_map(x_size: u32, y_size: u32) -> Vec<Vec<u32>> {
     return map;
 }
 
-fn _draw_line(map: PointMap, line: (Point, Point)) {
+fn _draw_line(map: &mut PointMap, line: (Point, Point)) {
     if line.0.x - line.1.x != 0 {
         _draw_horizontal_line(map, line);
     } else if line.0.y - line.1.y != 0 {
@@ -76,7 +102,7 @@ fn _draw_line(map: PointMap, line: (Point, Point)) {
     }
 }
 
-fn _draw_horizontal_line(map: PointMap, line: (Point, Point)) {
+fn _draw_horizontal_line(map: &mut PointMap, line: (Point, Point)) {
     let y = line.0.y;
 
     for i in line.0.x..line.1.x {
@@ -84,7 +110,7 @@ fn _draw_horizontal_line(map: PointMap, line: (Point, Point)) {
     }
 }
 
-fn _draw_vertical_line(map: PointMap, line: (Point, Point)) {
+fn _draw_vertical_line(map: &mut PointMap, line: (Point, Point)) {
     let x = line.0.x;
 
     for i in line.0.y..line.1.y {
@@ -121,6 +147,10 @@ fn main() -> Result<(), Error> {
     }
 
     let mut map: PointMap = PointMap::new(x_size, y_size);
+    for line in lines{
+        _draw_line(&mut map, line);
+    }
+    
 
     Ok(())
 }
